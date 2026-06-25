9873337d921b5801f294bf0bae49c0eb1c5ac5a5
Sara Valdéz <24sara.valdez@gmail.com>
2026-06-25 14:39:15 -0600

    Se implemento un boton revisar y se le dio funcionalidad a enter

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ let palabraEscondida = palabras[buscarIndice];
 
 let arrayhijos = document.querySelectorAll('.hijo');
 let mensajeError = document.querySelector('.mensaje-error');
+let botonRevisar = document.querySelector('#revisar')
 
 let indiceHijo = 0;
 let ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -21,44 +22,6 @@ let letras = [];
 
 const analizarPalabra = () => {
 
-    //   //aqui se va analizar la palabra en esta funcion
-
-    // this.removeEventListener('keyup', llenarTeclas)
-    // //Validadr teclas enter y borrar estas si deben funcionar
-
-    // if (letras.length == 5) {
-    //     let temporal = letras.slice(0, 6)
-    //     temporal = temporal.join();
-    //     temporal = temporal.replaceAll(',', '')
-    //     palabraEscondida == temporal ? finalizarJuego(banderita) : pintarColor()
-    // } else if (letras.length == 10) {
-    //     let temporal = letras.slice(5, 11)
-    //     temporal = temporal.join();
-    //     temporal = temporal.replaceAll(',', '')
-    //     palabraEscondida == temporal ? finalizarJuego(banderita) : pintarColor()
-    // } else if (letras.length == 15) {
-    //     let temporal = letras.slice(10, 16)
-    //     temporal = temporal.join();
-    //     temporal = temporal.replaceAll(',', '')
-    //     palabraEscondida == temporal ? finalizarJuego(banderita) : pintarColor()
-    // } else if (letras.length == 20) {
-    //     let temporal = letras.slice(15, 21)
-    //     temporal = temporal.join();
-    //     temporal = temporal.replaceAll(',', '')
-    //     palabraEscondida == temporal ? finalizarJuego(banderita) : pintarColor()
-    // } else if (letras.length == 25) {
-    //     let temporal = letras.slice(20, 16)
-    //     temporal = temporal.join();
-    //     temporal = temporal.replaceAll(',', '')
-    //     palabraEscondida == temporal ? finalizarJuego(banderita) : pintarColor()
-    // } else if (letras.length >= 30) {
-    //     let temporal = letras.slice(25, 31)
-    //     temporal = temporal.join();
-    //     temporal = temporal.replaceAll(',', '')
-    //     palabraEscondida == temporal ? banderita = true : banderita = false;
-    //     finalizarJuego(banderita)
-    // }
-
     let palabraEscrita = letras.join('');
 
     pintarColor()
@@ -67,13 +30,30 @@ const analizarPalabra = () => {
         banderita = true;
         finalizarJuego(banderita);
     } else if (filaActual === 5) {
-       banderita = false;
+        banderita = false;
         finalizarJuego(banderita);
     } else {
         filaActual++;
         letras = [];
     }
 };
+
+function mostrarMensaje(mensaje) {
+    mensajeError.classList.remove('d-none');
+    mensajeError.textContent = mensaje;
+    setTimeout(() => {
+        mensajeError.classList.add('d-none');
+    }, 3000);
+}
+
+botonRevisar.addEventListener('click', (event) => {
+    if (letras.length < 5) {
+        mostrarMensaje('Te faltan letras para completar la palabra');
+    } else {
+        analizarPalabra();
+    }
+
+})
 
 const llenarTeclas = (event) => {
     console.log("palabra esondida:", palabraEscondida);
@@ -91,35 +71,25 @@ const llenarTeclas = (event) => {
             arrayhijos[indiceHijo].textContent = tecla;
             letras.push(event.key.toLowerCase());
             indiceHijo++;
-
-            if (letras.length === 5) {
-                analizarPalabra();
-            }
         }
 
-    }
-    // else if (tecla === 'ENTER') {
-    //     if (letras.length < 5) {
-    //         mostrarError('Te faltan letras para completar la palabra');
-    //     }
-    // } else {
-    //     mostrarError('La tecla que seleccionó no está permitida');
-    // }
-};
+    } else if (tecla === 'ENTER') {
+        if (letras.length < 5) {
+            mostrarMensaje('Te faltan letras para completar la palabra');
+        } else {
+            analizarPalabra();
+        }
 
-function mostrarMensaje(mensaje) {
-    mensajeError.classList.remove('d-none');
-    mensajeError.textContent = mensaje;
-    setTimeout(() => {
-        mensajeError.classList.add('d-none');
-    }, 3000);
+    };
+
 }
-window.addEventListener('keyup', llenarTeclas);
+
+this.addEventListener('keyup', llenarTeclas);
 
 function pintarColor() {
 
-   let inicioFilaHTML = filaActual * 5;
-   console.log(inicioFilaHTML)
+    let inicioFilaHTML = filaActual * 5;
+    console.log(inicioFilaHTML)
 
     for (let i = 0; i < 5; i++) {
         let posicionCasilla = inicioFilaHTML + i;
@@ -137,20 +107,14 @@ function pintarColor() {
 }
 
 function finalizarJuego(resultado) {
-
-      console.log('Finalizado')
+    console.log('Finalizado')
     if (resultado) {
-         console.log('Felicidades!!!')
         mostrarMensaje('¡Felicidades! Adivinaste la palabra.');
     } else {
-        console.log('El juego termino')
         mostrarMensaje(`Perdiste. La palabra era: ${palabraEscondida.toUpperCase()}`);
     }
-    //El juego termino!!
-    //le envio true o false
-    //Realiza combios de ganodor o perdedor
-    //habilitar un boton reiniciar
-}
-//si banderita es true entonces se va ejecurtar la funcion la banderita hace que se detenga la funcion para verificar si es o no es la palabra si no es , entonces banderita va hacer true siempre si la palabra si es correcta entonces banderita sera false
 
-//banderita == true ? llenarTeclas(): console.log('se termino el juego ')
+
+    this.removeEventListener('keyup', llenarTeclas);
+
+}
